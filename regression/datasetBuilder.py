8e55d1d6b67a1233e5ab965b7c428e26aac526a7
Tavi <o.cazatavian@gmail.com>
2024-09-18 15:52:38 +0300

    made automatic dataset creation and cleaning (removal of outliers) for each category registered in the database and trained and saved reggression algorithms for each type of category

---
+++ regression/datasetBuilder.py
@@ -0,0 +1,49 @@
+import psycopg2
+import pandas as pd
+import os
+
+
+conn = psycopg2.connect(
+    host="localhost",  
+    database="product_administration",  
+    user="postgres",  
+    password="postgres"  
+)
+
+cur = conn.cursor()
+cur.execute('SELECT DISTINCT category FROM products')
+
+categories = cur.fetchall()
+formatted_categories = []
+for category in categories:
+    formatted_categories.append(str(category).replace(',','').replace('(','').replace(')',''))
+
+for category in formatted_categories:
+    try:
+        with open('dataset/dts_' + category.replace("'",'').replace(' ','_') + '_tmp.csv', 'a') as dataset:
+            cur.execute('SELECT price_products, price_products_2 FROM price_history_view_test WHERE category LIKE ' + str(category))
+            prices = cur.fetchall()
+            for price in prices:
+                price = str(price).replace(',','').replace('(','').replace(')','').replace("'",'')
+                price = price.strip().split(' ')
+                for i in range(2):
+                    price[i] = float(price[i])
+                if abs(price[0] - price[1]) < 500:
+                    dataset.write(str(price[0]) + ',' + str(price[1]) + '\n')
+
+        df = pd.read_csv('dataset/dts_' + category.replace("'",'').replace(' ','_') + '_tmp.csv', header=None, names=['Old_Price', 'New_Price'])
+        Q1 = df[['Old_Price', 'New_Price']].quantile(0.25)
+        Q3 = df[['Old_Price', 'New_Price']].quantile(0.75)
+
+        IQR = Q3 - Q1
+        outlier_step = 1.5 * IQR
+
+        df_filtered = df[~((df[['Old_Price', 'New_Price']] < (Q1 - outlier_step)) | (df[['Old_Price', 'New_Price']] > (Q3 + outlier_step))).any(axis=1)]
+        df_filtered.to_csv('dataset/dts_' + category.replace("'",'').replace(' ','_') + '.csv', index=False)
+
+        os.remove('dataset/dts_' + category.replace("'",'').replace(' ','_') + '_tmp.csv')
+    except Exception as e:
+        print('Someting went wrong: ' + str({e}))
+
+cur.close()
+conn.close()
---
+++ regression/modelTrainer.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import pickle
+import os
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, r2_score
+
+datasetDir = 'dataset/'
+
+for dataset in os.listdir(datasetDir):
+    dataset_path = os.path.join(datasetDir, dataset)
+    if os.path.isfile(dataset_path):
+        data = pd.read_csv(dataset_path)
+        if data.empty:
+            print(f"Skipping empty dataset: {dataset}")
+            continue
+
+        X = data[['Old_Price']]  
+        y = data['New_Price']    
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+        rf_regressor = RandomForestRegressor(n_estimators=100, random_state=42)
+
+        rf_regressor.fit(X_train, y_train)
+        
+        y_pred = rf_regressor.predict(X_test)
+
+        mse = mean_squared_error(y_test, y_pred)
+        r2 = r2_score(y_test, y_pred)
+
+        with open('performanceLogs.txt', 'a') as logs:
+            logs.write(f"Performance for category: " + '_'.join(dataset.split('_')[1:])  + '\n') 
+            logs.write(f"Mean Squared Error: {mse}" + '\n')
+            logs.write(f"R-Squared: {r2}" + '\n')
+            logs.write("-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n")
+
+        with open('models/' + str(dataset) + '.pkl', 'wb') as file:
+             pickle.dump(rf_regressor, file)
+    else:
+        print('Invalid Path -- ' + str(dataset_path) + ' does not exist')
